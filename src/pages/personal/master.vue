8594bd286bebf233020fbc0874905a4ca2fb15c6
bug <455928551@qq.com>
2017-12-18 17:34:42 +0800

    代理后台

--- src/pages/personal/master.vue
+++ src/pages/personal/master.vue
@@ -2,75 +2,90 @@
   <div class="moneyTemplateed">
     <div class="assetsDetailCls">
       <div class="rechargeLeft" @click="$router.go(-1);"></div>
-      <div class="assetsDeRight" @click="open('picker2')" size="large"><a href="javascript:void(0)"></a></div>
+      <div class="assetsDeRight assetsDeRightImg" @click="goContract"><a href="javascript:void(0)"></a></div>
       <div class="assetsTitle"><p>{{title}}</p></div>
       <div id="assetsDeList" class="assetsDeList">
-        <div class="master_nav">
-          <ul>
-            <li>
-              <p>我的徒弟数：<span class="masterNum">{{masterNum}}</span>名</p>
-            </li>
-            <li>
-              <p>有效投注人数：<span class="masterNum01">{{masterNum01}}</span>名</p>
-            </li>
-            <li>
-              <p>总盈亏：¥<span class="masteramout">{{masteramout}}</span></p>
-            </li>
-            <li>
-              <p>总佣金：¥<span class="masterMarry">{{masterMarry}}</span></p>
-            </li>
-          </ul>
+        <div class="" style="">
+            <div id="subnav" style="color:#FFDC99;margin-bottom: 0 !important">
+                <span @click="type = 0" :class="{on: type == 0}">佣金收益</span>
+                <span @click="type = 1" :class="{on: type == 1}">下级列表</span>
+            </div>
+            <div class="agentsTotal" v-if="type == 0">
+                <p>下级总数：<span></span>名</p>
+                <p>今日新增下级：<span></span>名</p>
+            </div>
+            <div class="agentsTotal" v-if="type == 1">
+                <p style="border-right: none !important" @click="opensYear"  size="large">选择年份
+                    <input type="text" :value="  value2 ? value2 : 2017" >
+                </p>
+                <p @click = "opensMoon">选择月份
+                    <input type="text" value="全部">
+                </p>
+            </div>
         </div>
-        <div style="height: 80%; overflow: hidden">
-          <div class="examineMian examineMian_master"
-               v-infinite-scroll="loadMore"
-               infinite-scroll-disabled="busy"
-               infinite-scroll-distance="50"
-               infinite-scroll-immediate-check="false"
-               style="height: 100%; overflow: auto;">
-            <ul class="master_ul">
-              <li>
-                <p>用户名</p>
-                <p>注册时间</p>
-                <p>盈亏</p>
-              </li>
-            </ul>
-            <ul class="master_ul" style="margin-bottom: 2rem;">
-              <li v-for="(master,index) in resultList">
-                <p><span>{{master.username}}</span></p>
-                <p><span>{{master.reg_time}}</span></p>
-                <p><span>￥{{master.sum}}</span></p>
-              </li>
-        <!--      <div class="noneDIV" v-if="betIndex === 0">
-                <p><img src="../../assets/images/noneImg.png"/></p>
-                <p>暂无记录哦～</p>
-              </div>-->
-            </ul>
-            <!--<span style="font-size: 0.5rem;color: #fff; padding-bottom: 0.5rem;" v-html="moreMsg"></span>-->
+          <div style="height: 80%; overflow: hidden;margin-top: 0.6rem;">
+              <div v-if="type == 0" class="examineMian examineMian_master"
+                   v-infinite-scroll="loadMore"
+                   infinite-scroll-disabled="busy"
+                   infinite-scroll-distance="50"
+                   infinite-scroll-immediate-check="false"
+                   style="height: 100%; overflow: auto;">
+                  <ul class="master_ul">
+                      <li class="list_tit">
+                          <p>结算时间段</p>
+                          <p>会员数</p>
+                          <p>盈亏额</p>
+                          <p>佣金</p>
+                      </li>
+                  </ul>
+                  <ul class="master_ul" style="margin-bottom: 2rem;">
+                      <li class="list_tit" v-for="(master,index) in resultList">
+                          <p><span>{{master.username}}</span></p>
+                          <p><span>{{master.reg_time}}</span></p>
+                          <p><span>￥{{master.sum}}</span></p>
+                          <p><span>￥{{master.sum}}</span></p>
+                      </li>
+                      <!--      <div class="noneDIV" v-if="betIndex === 0">
+                              <p><img src="../../assets/images/noneImg.png"/></p>
+                              <p>暂无记录哦～</p>
+                            </div>-->
+                  </ul>
+                  <!--<span style="font-size: 0.5rem;color: #fff; padding-bottom: 0.5rem;" v-html="moreMsg"></span>-->
+              </div>
+              <div v-if="type == 1" class="examineMian examineMian_master"
+                   v-infinite-scroll="loadMore"
+                   infinite-scroll-disabled="busy"
+                   infinite-scroll-distance="50"
+                   infinite-scroll-immediate-check="false"
+                   style="height: 100%; overflow: auto;">
+                  <ul class="master_ul">
+                      <li class="list_tit">
+                          <p style="width: 45%;">用户名</p>
+                          <p>盈亏额</p>
+                      </li>
+                  </ul>
+                  <ul class="master_ul" style="margin-bottom: 2rem;">
+                      <li class="list_tit" v-for="(master,index) in resultList">
+                          <p style="width: 45%;"><span>{{master.username}}</span></p>
+                          <p><span>{{master.reg_time}}</span></p>
+                      </li>
+                      <!--      <div class="noneDIV" v-if="betIndex === 0">
+                              <p><img src="../../assets/images/noneImg.png"/></p>
+                              <p>暂无记录哦～</p>
+                            </div>-->
+                  </ul>
+                  <!--<span style="font-size: 0.5rem;color: #fff; padding-bottom: 0.5rem;" v-html="moreMsg"></span>-->
+              </div>
           </div>
-        </div>
       </div>
     </div>
 
-    <!--<span v-if="busy">加载更多的数据...</span>-->
-    <!--<div class="Popup_up indexJust" v-if="showType">-->
-    <!--<div class="popup_upMian">-->
-    <!--<div class="popup_ulTitle popup_ulTitle_center">分类</div>-->
-    <!--<ul>-->
-    <!--<li v-for="(v, index) in showTypeItems" :class="{'on':numIndex == index}" @click="changeType(v, index)">{{v.name}}</li>-->
-    <!--</ul>-->
-    <!--<div @click="showType = !showType" class="delPopup">-->
-    <!--<img :src="delImg"/>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--</div>-->
-    <!--<dateChange v-if="resultList.length" :show="showType" :items="result"></dateChange>-->
-    <mt-datetime-picker
-            ref="picker2"
-            type="date"
-            v-model="value2"
-            @confirm="handleChange">
-    </mt-datetime-picker>
+      <!--<mt-datetime-picker-->
+              <!--ref="picker2"-->
+              <!--type="date"-->
+              <!--v-model="value2"-->
+              <!--@confirm="handleChange">-->
+      <!--</mt-datetime-picker>-->
   </div>
 </template>
 
@@ -82,26 +97,29 @@
 				result: [],
 				value2: null,
 				pickerValue: true,
-				title: "徒弟列表",
+				title: "代理后台",
 				resultList: [],
 				showType: false,
 				numIndex: 0,
 				showTypeItems: [],
 				delImg: require('../../assets/images/iconbtn2x.png'),
-				busy: false,
+                busy: false,
 				params: {
 					page: 1,
 					start_date: '',
 					end_date: ''
 				},
-				moreMsg: '',
-				betIndex: '',
-				masterNum: '',
-				masterNum01: '',
-				masteramout: '',
-				masterMarry: '',
-				current_page: 0,
-				last_page: 1,
+              type:0,
+              moreMsg: '',
+              betIndex: '',
+              masterNum: '',
+              masterNum01: '',
+              masteramout: '',
+              masterMarry: '',
+              current_page: 0,
+              last_page: 1,
+			  years: false,
+              moon:false
 			}
 		},
 
@@ -112,10 +130,16 @@
 		},
 
 		methods: {
+			opensYear () {
+				let that = this ;
+				that.years = true;
+            },
+            opensMoon () {
+                let that = this ;
+                that.years = true;
+            },
 			init() {
 				let that = this;
-//                console.log(!that.busy)
-				console.log("ssss:",that.current_page,that.last_page);
 				if (!that.busy) {
 					that.busy = true;
 					console.log("ssss:",that.current_page,that.last_page);
@@ -146,49 +170,43 @@
             that.last_page = parseInt(data.last_page);
 
 						if (parseInt(data.current_page) === data.last_page) {
-//							that._Util.showAlert(that, {content: '已经没有更多数据了'});
 							that.moreMsg = '';
 						}
 						that.busy = false;
-//												else {
-//													that.moreMsg = '正在加载更多的数据...';
-//												}
-//                        if (that.params.page === data.last_page && !data.data.length) {
-//                        	alert(1223)
-//													that.moreMsg = '已经没有更多数据了...';
-////                            that._Util.showAlert(that, {content: '已经没有更多数据了'});
-//                        }
 					});
 				}
 			},
 			open(picker) {
-				console.log("nsasda");
+//				console.log("nsasda");
 				this.$refs[picker].open();
 			},
-
-			handleChange(value) {
-				var that = this;
-				that.current_page = 0;
-				that.last_page = 1;
-				that.busy = false;
-				console.log("aaa:", value,this._Util.dateFormat(value,"yyyy-MM-dd"));
-				that.params.page = 1;
-				that.params.start_date = this._Util.dateFormat(value,"yyyy-MM-dd");
-                that.params.end_date = this._Util.dateFormat(value,"yyyy-MM-dd");
-				that.resultList=[];
-				that.init();
-			},
-			changeType(v, index) {
-				let that = this;
-				that.numIndex = index;
-				that.params.page = 1;
-				that.params.type = v.type;
-				that.resultList = [];
-				that.showType = false;
-                that.params.start_date = "";
-                that.params.end_data = "";
-				that.init();
-			},
+            goContract () {
+				let that = this ;
+	            that.$router.replace({name: 'agencyContract'});
+            },
+//			handleChange(value) {
+//				var that = this;
+//				that.current_page = 0;
+//				that.last_page = 1;
+//				that.busy = false;
+//				console.log("aaa:", value,this._Util.dateFormat(value,"yyyy-MM-dd"));
+//				that.params.page = 1;
+//				that.params.start_date = this._Util.dateFormat(value,"yyyy-MM-dd");
+//                that.params.end_date = this._Util.dateFormat(value,"yyyy-MM-dd");
+//				that.resultList=[];
+//				that.init();
+//			},
+//			changeType(v, index) {
+//				let that = this;
+//				that.numIndex = index;
+//				that.params.page = 1;
+//				that.params.type = v.type;
+//				that.resultList = [];
+//				that.showType = false;
+//                that.params.start_date = "";
+//                that.params.end_data = "";
+//				that.init();
+//			},
 
 			loadMore() {
 				let that = this;
@@ -217,9 +235,11 @@
   }*/
   .picker-items{
     height:6rem;
+      background: #fff !important;
   }
   .picker-slot-wrapper{
     height: auto !important;
+
   }
  .picker-toolbar .mint-datetime-action{
     font-size:0.7rem !important;
@@ -229,9 +249,10 @@
     height:1.5rem !important;
   }
   .picker-item {
-    /*height: 1.5rem !important;*/
-    /*line-height: 1.5rem !important;*/
     font-size: 0.6rem !important;
+      text-align: center !important;
+      height:1.5rem !important;
+      line-height:1.5rem !important;
   }
   .picker-item.picker-selected{
     font-size: 0.7rem !important;
