be4b036eb8878a961510240e3add981b43b1459c
1990218899@qq.com <123321qwe>
2017-12-27 10:48:11 +0800

    1、返回指令

--- src/assets/js/Utils.js
+++ src/assets/js/Utils.js
@@ -8,7 +8,7 @@ export default {
 
 	getUserInfo() {
   	let userInfo = this.getStorage('userInfo', true);
-		return userInfo ? userInfo.userInfo : {};
+		return userInfo ? (userInfo.userInfo || {}) : {};
 	},
   
   init() {
