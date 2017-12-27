1270bb44bb712537ffa47451cbb2bc999c5b680b
skyblue0 <www.1611625253@qq.com>
2017-12-27 11:50:53 +0800

    公告详情

--- src/pages/personal/annDetail.vue
+++ src/pages/personal/annDetail.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="annDetail">
-      <div class="commonNavBar positionFixed">
-        <div class="loginTitle"><p>公告详情</p></div>
-        <div class="loginIcon left" @click="$router.go(-1);"><a href="javascript:void(0)"></a></div>
-      </div>
-      <div class="annDetailsMian">
-        <div class="annDetailDate annDetailDateDetail">
-          <div class="annDetailTitle">
-            <p>{{result.title}}</p>
-            <p><span>{{result.create_time}}</span></p>
-          </div>
-          <!--v-html="result.content"-->
-          <div v-html="result.content" class="annDetailContentText"></div>
+    <div class="commonNavBar positionFixed">
+      <div class="backPassTitle"><p>公告详情</p></div>
+      <div class="loginIcon left flt" @click="$router.go(-1)"><a href="javascript:void(0)"></a></div>
+    </div>
+    <div class="annDetailsMian">
+      <div class="annDetailDate annDetailDateDetail">
+        <div class="annDetailTitle">
+          <p>{{result.title}}</p>
+          <p><span>{{result.create_time}}</span></p>
         </div>
+        <!--v-html="result.content"-->
+        <div v-html="result.content" class="annDetailContentText"></div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-//  import '../../assets/scss/default.scss';
-//  import '../../assets/scss/personal.scss';
+  //  import '../../assets/scss/default.scss';
+  //  import '../../assets/scss/personal.scss';
   
   
   export default {
